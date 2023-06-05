d20aaf06ddbca985cc738238702a4cb9b59a9c95
Shivansh1357 <120310888+Shivansh1357@users.noreply.github.com>
2023-06-05 13:12:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const players = ['John', 'Bob', 'Alice', 'Poppy'];
 
 const person = {
-  name: 'John Doe',
-  age: 80
-};
-
+      name: 'John Doe',
+      age: 80
+    };
 const team = players;
-const team1 = [...players];
-const cap1 = {...person};
+const team1 = players.slice();
 
-export {players, person, team, team1, cap1};
+//let copiedPerson = Object.assign({}, person);
+let cap1 = Object.assign({}, person);
+
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
